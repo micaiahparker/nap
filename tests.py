da203d57191ad75f3bbb1c0d8aa769f37308389e
Micaiah Parker <me@micaiahparker.com>
2017-04-07 12:52:28 -0400

    renamed test_model to model

--- tests.py
+++ tests.py
@@ -4,8 +4,8 @@ import falcon
 from pony.orm import Database, db_session
 from nap import NapMixin, NapAPI
 
-@pytest.fixture
-def test_model():
+@pytest.fixture(scope='function')
+def model():
     _db = Database()
 
     class TestModel(_db.Entity, NapMixin):
@@ -15,17 +15,17 @@ def test_model():
     _db.generate_mapping(create_tables=True)
     return TestModel
 
-def test_mixin_init(test_model):
+def test_mixin_init(model):
     with db_session:
-        assert test_model()
+        assert model()
 
-def test_mixin_put(test_model):
-    assert test_model.http_put() == {'id': 1}
+def test_mixin_put(model):
+    assert model.http_put() == {'id': 1}
     with pytest.raises(falcon.errors.HTTPConflict):
-        test_model.http_put(id=1)
+        model.http_put(id=1)
 
-def test_mixin_get(test_model):
+def test_mixin_get(model):
     with pytest.raises(falcon.errors.HTTPNotFound):
-        test_model.http_get(id=1)
-    test_model.http_put()
-    assert test_model.http_get(id=1) == {'id': 1}
+        model.http_get(id=1)
+    model.http_put()
+    assert model.http_get(id=1) == {'id': 1}
